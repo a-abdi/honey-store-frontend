1e13195309a9db649454ec435f5867fc5bbd5474
a-badi <ali.abdi.1991@gmail.com>
2023-01-19 23:46:38 +0330

    install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+ // tailwind.config.js
+ module.exports = {
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+   theme: {
+     extend: {},
+   },
+  
+   plugins: [],
+ }
