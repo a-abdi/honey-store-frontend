d30bbfea851948438897f6da0c397c5db0f901a7
a-badi <ali.abdi.1991@gmail.com>
2023-03-15 17:45:51 +0330

    review admin link

--- src/layouts/AppLayoutList/AppLayoutAdmin.vue
+++ src/layouts/AppLayoutList/AppLayoutAdmin.vue
@@ -2,26 +2,10 @@
     <div class="h-screen overflow-hidden">
         <header/>
         <AdminLinks/>
-        <slot />
+        <slot/>
     </div>
 </template>
 
-<script lang="ts">
-import { computed } from 'vue'
-import AdminLinks from '../Links/AdminLinks.vue'
-
-export default {
-    name: "AppLayoutHome",
-
-    components: {
-        AdminLinks,
-    },
-
-    setup () {
-
-        return {
-            // isLoggined: computed( () => store.getters['adminAuth/checkAuth'])
-        }
-    }
-}
+<script setup lang="ts">
+    import AdminLinks from '../Links/AdminLinks.vue';
 </script>
--- src/layouts/Links/AdminLinks.vue
+++ src/layouts/Links/AdminLinks.vue
@@ -1,21 +1,27 @@
 <template>
-    <nav class="sticky top-0 bg-gray-100">
-        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    <nav class="sticky top-0 right-0 left-0 bg-gray-100">
+        <div class="mx-auto px-2 sm:px-6 lg:px-8">
             <!-- Mobile menu, show/hide based on menu state. -->
-        <div v-show="mainMenu"  class="sm:hidden" id="mobile-menu">
-            <div class="px-2 pt-2 pb-3 space-y-1">
-                <router-link to="/admin/dashboard" class="bg-gray-50 text-gray-900 block px-3 py-2 rounded-md text-base font-medium" aria-current="page">داشبورد</router-link>
+            <div v-show="mainMenu" class="sm:hidden" id="mobile-menu">
+                <div class="px-2 pt-2 pb-3 space-y-1">
+                    <router-link to="/admin/dashboard" class="bg-gray-50 text-gray-900 block px-3 py-2 rounded-md text-base font-medium" aria-current="page">داشبورد</router-link>
+                </div>
             </div>
-        </div>
             <div class="relative flex items-center justify-between h-14">
-                <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <button class="bg-gray-50 p-1 rounded-full text-gray-600 hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-300 focus:ring-gray-200">
-                        <span class="sr-only">View notifications</span>
-                        <!-- Heroicon name: outline/bell -->
-                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                        </svg>
-                    </button>
+                <div class="flex items-center justify-center sm:items-stretch sm:justify-start">
+                    <div class="hidden sm:block sm:ml-6">
+                        <div class="flex space-x-4">
+                            <router-link to="/admin/dashboard" class="bg-gray-50 text-gray-900 px-3 py-2 rounded-md text-sm font-medium" aria-current="page">داشبورد</router-link>
+                            <router-link to="/" class="text-gray-600 hover:bg-gray-50 hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium"> خانه </router-link>
+                        </div>
+                    </div>
+                </div>
+                <div class="absolute inset-y-0 right-0 flex items-center sm:hidden">
+                    <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path fill="none" stroke="#878787" stroke-linecap="round" stroke-width="2" d="M5 6h14M5 12h14M5 18h14"/>
+                    </svg>
+                </div>
+                <div class="absolute inset-y-0 left-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                     <!-- Profile dropdown -->
                     <div class="ml-3 relative">
                         <div v-click-away="closeUserMenu">
@@ -26,69 +32,29 @@
                                 </svg>
                             </button>
                         </div>
-                        <!--
-                            Dropdown menu, show/hide based on menu state.
-
-                            Entering: "transition ease-out duration-100"
-                            From: "transform opacity-0 scale-95"
-                            To: "transform opacity-100 scale-100"
-                            Leaving: "transition ease-in duration-75"
-                            From: "transform opacity-100 scale-100"
-                            To: "transform opacity-0 scale-95"
-                        -->
-                        <div v-show="userMenu" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-gray-50 ring-1 ring-gray-300 ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                            <!-- Active: "bg-gray-100", Not Active: "" -->
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-600" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-600" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-                            <button class="text-left w-full focus:outline-none block px-4 py-2 text-sm text-gray-600" role="menuitem" tabindex="-1" id="user-menu-item-2">
-                                Sign out
+                        <div v-show="userMenu" class="origin-top-right absolute left-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-gray-50 ring-1 ring-gray-300 ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-600" role="menuitem" tabindex="-1" id="user-menu-item-0">پروفایل</a>
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-600" role="menuitem" tabindex="-1" id="user-menu-item-1">تنظیمات</a>
+                            <button class="text-right w-full focus:outline-none block px-4 py-2 text-sm text-gray-600" role="menuitem" tabindex="-1" id="user-menu-item-2">
+                                خروج
                             </button>
                         </div>
                     </div>
-                </div>
-                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                    <!-- Mobile menu button-->
-                    <button @click="mainMenu = !mainMenu" type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-800 hover:text-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-                        <span class="sr-only">Open main menu</span>
-                        <!--
-                            Icon when menu is closed.
-
-                            Heroicon name: outline/menu
-
-                            Menu open: "hidden", Menu closed: "block"
-                        -->
-                        <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                        <!--
-                            Icon when menu is open.
-
-                            Heroicon name: outline/x
-
-                            Menu open: "block", Menu closed: "hidden"
-                        -->
-                        <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    <button class="bg-gray-50 p-1 rounded-full text-gray-600 hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-300 focus:ring-gray-200">
+                        <span class="sr-only">View notifications</span>
+                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                         </svg>
                     </button>
                 </div>
-                <div class="flex-r flex items-center justify-center sm:items-stretch sm:justify-start">
-                    <div class="hidden sm:block sm:ml-6">
-                        <div class="flex space-x-4">
-                            <router-link to="/admin/dashboard" class="bg-gray-50 text-gray-900 px-3 py-2 rounded-md text-sm font-medium" aria-current="page">داشبورد</router-link>
-                            <router-link to="/" class="text-gray-600 hover:bg-gray-50 hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium"> خانه </router-link>
-                        </div>
-                    </div>
-                </div>
-                
             </div>
         </div>
     </nav>
 </template>
 
 <script setup lang="ts">
-    import { ref } from 'vue'
-    const userMenu = ref(false)
-    const mainMenu = ref(false)
-    const closeUserMenu = () => userMenu.value = false
+    import { ref } from 'vue';
+    const userMenu = ref(false);
+    const mainMenu = ref(false);
+    const closeUserMenu = () => userMenu.value = false;
 </script>
