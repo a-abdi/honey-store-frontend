1aee74754ab584e1842b21e829f617afeddb1837
a-badi <ali.abdi.1991@gmail.com>
2023-05-19 16:37:39 +0330

    implement property typing

---
+++ src/common/typings/property.typing.ts
@@ -0,0 +1,17 @@
+import type { Category } from "./category.typings";
+import type { CommonResponseData, CommonResponseDataOptional } from "./common.typings";
+
+export interface newProperty {
+    label: string;
+    type: string;
+    unit?: string[];
+    category?: string[];
+};
+
+export interface Property extends Omit<newProperty, 'category'>, CommonResponseData {
+    category: string | Category;
+};
+
+export interface PropertyData extends CommonResponseDataOptional {
+    data: Property | null;
+};
