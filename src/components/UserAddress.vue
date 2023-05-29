ccfe6fbd6a33e87fb2d8a4e15f406e0fc0ea7e38
a-badi <ali.abdi.1991@gmail.com>
2023-05-29 22:33:00 +0330

    install vueuse and use OnClickOutside

--- src/components/UserAddress.vue
+++ src/components/UserAddress.vue
@@ -4,98 +4,100 @@
             <div v-show="showDialog" class="fixed z-10 inset-0 overflow-y-auto bg-gray-400 bg-opacity-25">
                 <div class="w-full">
                     <div class="relative form-design sm:my-6 lg:w-1/2 md:w-3/4 w-full bg-white">
-                        <button @click="emit('cancel')" class="absolute top-4 left-4 text-violet-600 focus:outline-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4l-4.9 4.9q-.275.275-.7.275q-.425 0-.7-.275q-.275-.275-.275-.7q0-.425.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7q0-.425.275-.7q.275-.275.7-.275q.425 0 .7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275q.425 0 .7.275q.275.275.275.7q0 .425-.275.7L13.4 12l4.9 4.9q.275.275.275.7q0 .425-.275.7q-.275.275-.7.275q-.425 0-.7-.275Z"/></svg>
-                        </button>
-                        <form @submit.prevent="">
-                            <div class="sm:mx-6 my-8 px-4 sm:px-0 text-gray-600">
-                                <div class="sm:mb-3 mb-1 text-violet-600">
-                                    آدرس پستی
-                                </div>
-                                <div class="w-full">
-                                    <textarea v-model="newAddress.postalAddress" class="p-2 text-gray-600 resize-y border rounded-md w-full h-8 sm:h-12 md:h-16 xl:h-20 focus:outline-none focus:ring-200"></textarea>
-                                </div>
-                            </div>
-                            <div class="sm:flex items-center justify-between sm:my-6">
-                                <div class="w-full sm:mx-6 sm:my-0 my-6 px-4 sm:px-0 text-gray-600">
+                        <OnClickOutside @trigger="emit('cancel')">
+                            <button @click="emit('cancel')" class="absolute top-4 left-4 text-violet-600 focus:outline-none">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4l-4.9 4.9q-.275.275-.7.275q-.425 0-.7-.275q-.275-.275-.275-.7q0-.425.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7q0-.425.275-.7q.275-.275.7-.275q.425 0 .7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275q.425 0 .7.275q.275.275.275.7q0 .425-.275.7L13.4 12l4.9 4.9q.275.275.275.7q0 .425-.275.7q-.275.275-.7.275q-.425 0-.7-.275Z"/></svg>
+                            </button>
+                            <form @submit.prevent="">
+                                <div class="sm:mx-6 my-8 px-4 sm:px-0 text-gray-600">
                                     <div class="sm:mb-3 mb-1 text-violet-600">
-                                        استان
+                                        آدرس پستی
                                     </div>
                                     <div class="w-full">
-                                        <select v-model="newAddress.province" @change="updateCity" name="province" autofocus="true" id="province" aria-placeholder="select province" class="w-full py-2 bg-white text-gray-600 form-input text-right">
-                                            <option v-for="province in provinces" :key="province.id" :value="province.name">{{ province.name }}</option>
-                                        </select>                        
+                                        <textarea v-model="newAddress.postalAddress" class="p-2 text-gray-600 resize-y border rounded-md w-full h-8 sm:h-12 md:h-16 xl:h-20 focus:outline-none focus:ring-200"></textarea>
                                     </div>
                                 </div>
-                                <div class="w-full sm:mx-6 sm:my-0 my-6 px-4 sm:px-0 text-gray-600">
-                                    <div class="sm:mb-3 mb-1 text-violet-600">
-                                        شهر
+                                <div class="sm:flex items-center justify-between sm:my-6">
+                                    <div class="w-full sm:mx-6 sm:my-0 my-6 px-4 sm:px-0 text-gray-600">
+                                        <div class="sm:mb-3 mb-1 text-violet-600">
+                                            استان
+                                        </div>
+                                        <div class="w-full">
+                                            <select v-model="newAddress.province" @change="updateCity" name="province" autofocus="true" id="province" aria-placeholder="select province" class="w-full py-2 bg-white text-gray-600 form-input text-right">
+                                                <option v-for="province in provinces" :key="province.id" :value="province.name">{{ province.name }}</option>
+                                            </select>                        
+                                        </div>
                                     </div>
-                                    <div class="w-full">
-                                        <select v-model="newAddress.city" name="city" autofocus="true" id="city" aria-placeholder="select city" class="w-full py-2 bg-white text-gray-600 form-input text-right">
-                                            <option v-for="city in citiesProvince" :key="city.id" :value="city.name">{{ city.name }}</option>
-                                        </select>   
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="sm:flex items-center justify-between sm:my-6">
-                                <div class="w-full sm:mx-6 sm:my-0 my-6 px-4 sm:px-0 text-gray-600">
-                                    <div class="sm:mb-3 mb-1 text-violet-600">
-                                        پلاک
-                                    </div>
-                                    <div class="w-full">
-                                        <input v-model="newAddress.plaque" name="plaque" id="plaque" class="form-input">
-                                    </div>
-                                </div>
-                                <div class="w-full sm:mx-6 sm:my-0 my-6 px-4 sm:px-0 text-gray-600">
-                                    <div class="sm:mb-3 mb-1 text-violet-600">
-                                        کد پستی
-                                    </div>
-                                    <div class="w-full">
-                                        <input v-model="newAddress.postalCode" name="postalCode" id="postalCode" class="form-input">
+                                    <div class="w-full sm:mx-6 sm:my-0 my-6 px-4 sm:px-0 text-gray-600">
+                                        <div class="sm:mb-3 mb-1 text-violet-600">
+                                            شهر
+                                        </div>
+                                        <div class="w-full">
+                                            <select v-model="newAddress.city" name="city" autofocus="true" id="city" aria-placeholder="select city" class="w-full py-2 bg-white text-gray-600 form-input text-right">
+                                                <option v-for="city in citiesProvince" :key="city.id" :value="city.name">{{ city.name }}</option>
+                                            </select>   
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="sm:flex items-center justify-between sm:my-6">
-                                <div class="w-full sm:mx-6 sm:my-0 my-6 px-4 sm:px-0 text-gray-600">
-                                    <div class="sm:mb-3 mb-1 text-violet-600">
-                                        نام گیرنده
+                                <div class="sm:flex items-center justify-between sm:my-6">
+                                    <div class="w-full sm:mx-6 sm:my-0 my-6 px-4 sm:px-0 text-gray-600">
+                                        <div class="sm:mb-3 mb-1 text-violet-600">
+                                            پلاک
+                                        </div>
+                                        <div class="w-full">
+                                            <input v-model="newAddress.plaque" name="plaque" id="plaque" class="form-input">
+                                        </div>
                                     </div>
-                                    <div class="w-full">
-                                        <input v-model="newAddress.recipient.firstName" name="recipientFirstName" id="recipientFirstName" class="form-input">
-                                    </div>
-                                </div>
-                                <div class="w-full sm:mx-6 sm:my-0 my-6 px-4 sm:px-0 text-gray-600">
-                                    <div class="sm:mb-3 mb-1 text-violet-600">
-                                        نام خانوادگی گیرنده
-                                    </div>
-                                    <div class="w-full">
-                                        <input v-model="newAddress.recipient.lastName" name="recipientLastName" id="recipientLastName" class="form-input">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="flex items-center justify-between sm:my-6">
-                                <div class="w-full sm:mx-6 px-4 sm:px-0 text-gray-600">
-                                    <div class="sm:mb-3 mb-1 text-violet-600">
-                                        تلفن گیرنده
-                                    </div>
-                                    <div class="w-full">
-                                        <input v-model="newAddress.recipient.phoneNumber" name="recipientPhoneNumber" id="recipientPhoneNumber" class="form-input">
+                                    <div class="w-full sm:mx-6 sm:my-0 my-6 px-4 sm:px-0 text-gray-600">
+                                        <div class="sm:mb-3 mb-1 text-violet-600">
+                                            کد پستی
+                                        </div>
+                                        <div class="w-full">
+                                            <input v-model="newAddress.postalCode" name="postalCode" id="postalCode" class="form-input">
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="sm:mx-6 my-6 sm:px-0 px-4">
-                                <button @click="updateAddress" class="btn-violet w-full p-2">
-                                    ثبت آدرس
-                                </button>
-                            </div>
-                        </form>
-                        <Message class="absolute bottom-0 right-0 bg-gray-300" 
-                            :message="page.message"
-                            :showMessage="page.showMessage"
-                            :typeMessage="page.typeMessage"
-                            @fadeMessage="page.showMessage = false" 
-                        />
+                                <div class="sm:flex items-center justify-between sm:my-6">
+                                    <div class="w-full sm:mx-6 sm:my-0 my-6 px-4 sm:px-0 text-gray-600">
+                                        <div class="sm:mb-3 mb-1 text-violet-600">
+                                            نام گیرنده
+                                        </div>
+                                        <div class="w-full">
+                                            <input v-model="newAddress.recipient.firstName" name="recipientFirstName" id="recipientFirstName" class="form-input">
+                                        </div>
+                                    </div>
+                                    <div class="w-full sm:mx-6 sm:my-0 my-6 px-4 sm:px-0 text-gray-600">
+                                        <div class="sm:mb-3 mb-1 text-violet-600">
+                                            نام خانوادگی گیرنده
+                                        </div>
+                                        <div class="w-full">
+                                            <input v-model="newAddress.recipient.lastName" name="recipientLastName" id="recipientLastName" class="form-input">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="flex items-center justify-between sm:my-6">
+                                    <div class="w-full sm:mx-6 px-4 sm:px-0 text-gray-600">
+                                        <div class="sm:mb-3 mb-1 text-violet-600">
+                                            تلفن گیرنده
+                                        </div>
+                                        <div class="w-full">
+                                            <input v-model="newAddress.recipient.phoneNumber" name="recipientPhoneNumber" id="recipientPhoneNumber" class="form-input">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="sm:mx-6 my-6 sm:px-0 px-4">
+                                    <button @click="updateAddress" class="btn-violet w-full p-2">
+                                        ثبت آدرس
+                                    </button>
+                                </div>
+                            </form>
+                            <Message class="absolute bottom-0 right-0 bg-gray-300" 
+                                :message="page.message"
+                                :showMessage="page.showMessage"
+                                :typeMessage="page.typeMessage"
+                                @fadeMessage="page.showMessage = false" 
+                            />
+                        </OnClickOutside>
                     </div>
                 </div>
             </div>
@@ -104,7 +106,6 @@
 </template>
 
 <script setup lang="ts">
-import router from '@/router';
 import { reactive, ref, type Ref, onMounted } from 'vue';
 import { useUserStore } from '@/stores/user';
 import { TypeMessage, type Page, type City} from '@/common/typings/common.typings';
@@ -116,6 +117,7 @@ import provinces from '@/assets/address/provinces.json';
 import cities from '@/assets/address/cities.json';
 import type { UserAddress } from '@/common/typings/user.typing';
 import { storeToRefs } from 'pinia';
+import { OnClickOutside } from '@vueuse/components'
 const userStore = useUserStore();
 const citiesProvince = ref([]) as Ref<City[]>;
 const config = getUserConfig();
--- src/components/user-link/ProfileLink.vue
+++ src/components/user-link/ProfileLink.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="relative">
-        <UserIcone  class="w-8 h-8 text-gray-400 cursor-pointer"/>
-        <div class="absolute top-7 left-0 border border-gray-300 rounded-md shadow-lg bg-white h-auto overflow-auto max-h-128 w-64">
-
-        </div>
+        <UserIcone @click="showProfile = true" class="w-8 h-8 text-gray-400 cursor-pointer"/>
+        <OnClickOutside @trigger="showProfile = false">
+            <div v-if="showProfile" class="absolute top-7 left-0 border border-gray-300 rounded-md shadow-lg bg-white h-auto overflow-auto max-h-128 w-64">
+                Hello
+            </div>
+        </OnClickOutside>
     </div>
 </template>
 <script lang="ts" setup>
 import UserIcone from '@/components/icons/UserLink.vue';
-
+import { OnClickOutside } from '@vueuse/components'
+import { ref } from 'vue';
+const showProfile = ref(false);
 </script>
